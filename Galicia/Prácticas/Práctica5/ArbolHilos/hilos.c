128eca71e42fdaf033b3ffb162c35c73b9880d90
Joel Romero <joelrg1288@gmail.com>
2017-05-06 02:53:02 -0500

    Se corrige árbol, y se ponen los hijos finales pedidos, 15, 10 y 5.

--- Galicia/Prácticas/Práctica5/ArbolHilos/hilos.c
+++ Galicia/Prácticas/Práctica5/ArbolHilos/hilos.c
@@ -7,26 +7,13 @@
 void * hiloNivel2 (void *arg);
 void * hiloNivel3 (void *arg);
 void * hiloTerminal (void *arg);
-int i;
 
-/////////////////////////////////////////////////////////////////////////
-////                         hilos.c	                             ////
-////																 ////
-////																 ////
-//// Creación de un árbol de hilos a partir de un proceso hijo, el   ////
-//// cual comienza creando 15 hilos iniciales, que a su vez crean 	 ////
-//// 10 hilos cada uno de esos 15 hilos, y a su vez, esos 150 crean  ////
-//// los ultimos 5 hilos. Los hilos terminales imprimen "Practica 5" ////
-//// mientras que los otros hilos imprimen su identificador y su 	 ////
-//// nivel al que pertenecen.										 ////
-////																 ////
-////																 ////
-//// Autor: Romero Gamarra Joel Mauricio                    		 ////
-/////////////////////////////////////////////////////////////////////////
 
-int main ()
+
+int main (void)
 {
   int id_proc, status;
+  int i, j;
   system("clear");
   printf("\n\n\n\tIDENTIFICADOR\t\t\tNIVEL\n\n");
   id_proc = fork ();
@@ -34,11 +21,11 @@ int main ()
   {
   	printf("\t%d\t\t\t\t1\t\t<- Proceso hijo\n", getpid());
 	pthread_t id_Hilo[15];
-  	for (i = 0; i < 3; i++)
+  	for (i = 0; i < 15; i++)
   	{
 	    pthread_create (&id_Hilo[i], NULL, (void *)hiloNivel2, NULL);		// CreaciÃ³n de los primeros 15 hilos
   	}
-  	for (i = 0; i < 3; i++)
+  	for (i = 0; i < 15; i++)
   	{
   		pthread_join(id_Hilo[i], NULL);
   	}
@@ -56,22 +43,23 @@ void * hiloNivel2 (void *arg)
 {
 	printf ("\t%lu\t\t\t2\n", pthread_self());
 	pthread_t id_Hilo[10];
-  	for (i = 0; i < 2; i++)
+	int i, j;
+  	for (i = 0; i < 10; i++)
   	{
     	pthread_create (&id_Hilo[i], NULL, (void *)hiloNivel3, NULL);				// Se crean los 10 hilos
 	}
-	for (i = 0; i < 2; ++i)
+	for (i = 0; i < 10; ++i)
 	{
 		pthread_join(id_Hilo[i], NULL);
 	}
-	//pthread_exit(NULL);
-	return NULL;
+	pthread_exit(NULL);
 }
 
 void * hiloNivel3 (void *arg)
 {
-	printf ("\t%lu\t\t\t3\t\t<-NIVEL 3\n", pthread_self());
+	printf ("\t%lu\t\t\t3\n", pthread_self());
 	pthread_t id_Hilo[5];
+	int i, j;
   	for (i = 0; i < 5; i++)
   	{
 	    pthread_create (&id_Hilo[i], NULL, (void *)hiloTerminal, NULL);			// CreaciÃ³n de los Ãºltimos 5 hilos
@@ -80,13 +68,11 @@ void * hiloNivel3 (void *arg)
   	{
   		pthread_join (id_Hilo[i], NULL);											//Espera a que se ejecuten los hilos
   	}
-  	//pthread_exit(NULL);
-  	return NULL;
+  	pthread_exit(NULL);
 }
 
 void * hiloTerminal (void *arg)
 {
-  printf ("\tPractica 5\n");
+  printf ("\tPractica 5\t\t\t4\n");
   pthread_exit(NULL);
 }
-
