e1c37cf9c9610087d7f809cd4da842c8b3ecc354
Joel Romero <joelrg1288@gmail.com>
2017-05-08 12:20:21 -0500

    Se hace modular el proyecto completo para imprimir, generar y crear matrices dinámicas.

--- Rangel/Prácticas/Práctica4/Matrices.c
+++ Rangel/Prácticas/Práctica4/Matrices.c
@@ -9,6 +9,10 @@ typedef struct Matrices		// Definición de la estructura de argumentos para la f
   int **m1, **m2,**m3, n;
 }matrices;
 
+void crearMatrices (void *args);
+void GenerarMatrices (void *args);
+void ImprimirMatriz (int **m1, int x, int n);
+
 ////////////////////////////////////////////////////////////////////////////////
 ////								Matrices.c 								////
 ////																		////
@@ -29,54 +33,60 @@ int main(int argc, char const *argv[])
 	system ("clear");
 	printf ("Digite el tamaño de la matriz:\t");
 	scanf ("%d",&matriz->n);
-	matriz->m1 = (int **)malloc(sizeof(int *) * matriz->n);
-	matriz->m2 = (int **)malloc(sizeof(int *) * matriz->n);
-	matriz->m3 = (int **)malloc(sizeof(int *) * matriz->n);
-	for (int i = 0; i < (matriz->n); i++)
-	{
-		matriz->m1[i] = (int *)malloc(sizeof(int *) * matriz->n);
-		matriz->m2[i] = (int *)malloc(sizeof(int *) * matriz->n);
-		matriz->m3[i] = (int *)malloc(sizeof(int *) * matriz->n);
-	}
-	srand (time (NULL));
-	for (i = 0; i < (matriz->n); i++)			/*SE GENERAN AMBAS MATRICES ALEATORIAMENTE*/
-	{
-		for (j = 0; j < (matriz->n); j++)
-		{
-			matriz->m1[i][j] = rand()%10;
-			matriz->m2[i][j] = rand()%10;
-		}
-	}
-
-	printf("\n\nMatriz 1\n\n");
-	for (i = 0; i < (matriz->n); i++)
-	{
-		for (j = 0; j < (matriz->n); j++)
-		{
-			printf("%d ",matriz->m1[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n\nMatriz 2\n\n");
-	for (i = 0; i < (matriz->n); i++)
-	{
-		for (j = 0; j < (matriz->n); j++)
-		{
-			printf("%d ",matriz->m2[i][j]);
-		}
-		printf("\n");
-	}
+	crearMatrices ((void *)matriz);
+	GenerarMatrices((void *)matriz);
+	ImprimirMatriz(matriz->m1, 1, matriz->n);
+	ImprimirMatriz(matriz->m2, 2, matriz->n);
 	pthread_create (&id_Hilo, NULL, (void *)hilo, (void *)matriz);		//Creación del hilo
   	pthread_join(id_Hilo, (void *)&matriz);
-  	printf("\n\nMatriz resultante\n\n");
-  	for (i = 0; i < (matriz->n); i++)
+  	ImprimirMatriz(matriz->m3, 3, matriz->n);
+  	return 0;
+}
+
+void crearMatrices (void *args)
+{
+	int i;
+	matrices * matriz = (matrices *)args;			//Casteo de los argumentos
+	matriz->m1 = (int **)malloc(sizeof(int *) * (matriz->n));
+	matriz->m2 = (int **)malloc(sizeof(int *) * (matriz->n));
+	matriz->m3 = (int **)malloc(sizeof(int *) * (matriz->n));
+	for (int i = 0; i < (matriz->n); i++)
+	{
+		matriz->m1[i] = (int *)malloc(sizeof(int *) * (matriz->n));
+		matriz->m2[i] = (int *)malloc(sizeof(int *) * (matriz->n));
+		matriz->m3[i] = (int *)malloc(sizeof(int *) * (matriz->n));
+	}
+}
+
+void GenerarMatrices(void *args)
+{
+	int i, j;
+	matrices * matriz = (matrices *)args;			//Casteo de los argumentos
+	srand (time (NULL));
+	for (i = 0; i < (matriz->n); i++)
 	{
 		for (j = 0; j < (matriz->n); j++)
 		{
-			printf("%d ",matriz->m3[i][j]);
+			matriz->m1[i][j] = rand()%5;
+			matriz->m2[i][j] = rand()%5;
+		}
+	}
+	return;
+}
+
+void ImprimirMatriz(int **m1, int x, int n)
+{
+	int i, j;
+	printf("\n\nMatriz %d\n", x);
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			printf("%d ",m1[i][j]);
 		}
 		printf("\n");
 	}
+	return;
 }
 
 void * hilo (void *arg)
