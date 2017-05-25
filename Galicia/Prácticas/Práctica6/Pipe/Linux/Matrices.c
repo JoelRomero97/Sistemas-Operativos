a8d56c9745bc33fee0a148eacf025b59f4730bf2
Joel Romero <joelrg1288@gmail.com>
2017-05-25 10:05:35 -0500

    Se agrega opcion de multiplicar matrices.

--- Galicia/Prácticas/Práctica6/Pipe/Linux/Matrices.c
+++ Galicia/Prácticas/Práctica6/Pipe/Linux/Matrices.c
@@ -14,6 +14,7 @@ void crearMatrices (void *args, int n);
 void GenerarMatrices (int **m1, int **m2, int n);
 void ImprimirMatriz (int **m1, int x, int n);
 int ** SumaMatrices (int **m1, int **m2, int **m3, int n);
+int ** MultiplicaMatrices (int **m1, int **m2, int **m3, int n);
 
 ////////////////////////////////////////////////////////////////////////////////
 ////								Matrices.c 								////
@@ -62,13 +63,13 @@ int main (void)
 	}
 	if (fork () == 0)
 	{
-		read (suma[0], matrizSuma, sizeof (matrices *));
-		printf("\n\nSe recibieron las matrices\n\n");
-		ImprimirMatriz (matrizSuma->m1, 1, n);
-		ImprimirMatriz (matrizSuma->m2, 2, n);
+		read (suma[0], matrizSuma, sizeof (matrices *));				//Recibimos las matrices
+		printf("\n\nSe recibieron las matrices para sumar\n\n");
+		//ImprimirMatriz (matrizSuma->m1, 1, n);
+		//ImprimirMatriz (matrizSuma->m2, 2, n);
 		matrizSuma->m3 = SumaMatrices (matrizSuma->m1, matrizSuma->m2, matrizSuma->m3, n);
-		printf("\n\nLa matriz resultante de la suma antes de escribirla en el pipe, es:\n\n");
-		ImprimirMatriz (matrizSuma->m3, 3, n);
+		//printf("\n\nLa matriz resultante de la suma antes de escribirla en el pipe, es:\n\n");
+		//ImprimirMatriz (matrizSuma->m3, 3, n);
 		for (int i = 0; i < n; i ++)
 		{
 			for (int j = 0; j < n; j ++)
@@ -77,12 +78,32 @@ int main (void)
 			}
 		}
 		write (sumaResultado[1], resSuma, sizeof (resSuma));
+		if ((fork ()) == 0)
+		{
+			read (mult[0], matrizMult, sizeof (matrices *));				//Recibimos las matrices
+			printf("\n\nSe recibieron las matrices para multiplicar\n\n");
+			ImprimirMatriz (matrizMult->m1, 1, n);
+			ImprimirMatriz (matrizMult->m2, 2, n);
+			matrizMult->m3 = MultiplicaMatrices (matrizMult->m1, matrizMult->m2, matrizMult->m3, n);
+			printf("\n\nLa matriz resultante de la multiplicacion antes de escribirla en el pipe, es:\n\n");
+			ImprimirMatriz (matrizMult->m3, 3, n);
+			for (int i = 0; i < n; i ++)
+			{
+				for (int j = 0; j < n; j ++)
+				{
+					resMult[i][j] = matrizMult->m3[i][j];
+				}
+			}
+			write (multResultado[1], resMult, sizeof (resMult));
+		}
 	}else
 	{
 		write (suma[1], matrizSuma, sizeof(matrices *));			//Mandamos 2 matrices para sumarlas
+		write (mult[1], matrizMult, sizeof (matrices *));			//Mandamos 2 matrices para multiplicarlas
 		while(wait(&status)>0);
 		read (sumaResultado [0], resSuma, sizeof (resSuma));
-		printf("\n\nSe recibió la suma de las matrices que es:\n");
+		read (multResultado [0], resMult, sizeof (resMult));
+		//printf("\n\nSe recibió la suma de las matrices que es:\n");
 		for (int i = 0; i < n; i ++)
 		{
 			for (int j = 0; j < n; j ++)
@@ -90,7 +111,15 @@ int main (void)
 				matriz->m3[i][j] = resSuma[i][j];
 			}
 		}
-		ImprimirMatriz (matriz->m3, 3, n);
+		printf("Se recibio la multiplicacion de matrices que es:\n\n");
+		for (int i = 0; i < n; i ++)
+		{
+			for (int j = 0; j < n; j ++)
+			{
+				matriz->m2[i][j] = resMult[i][j];
+			}
+		}
+		ImprimirMatriz (matriz->m2, 3, n);
 	}
 }
 
@@ -147,6 +176,23 @@ int ** SumaMatrices (int **m1, int **m2, int **m3, int n)
 		for (j = 0; j < n; j++)
 		{
 			m3[i][j] = m1[i][j] + m2[i][j];
+		}
+	}
+	return m3;
+}
+
+int ** MultiplicaMatrices (int **m1, int **m2, int **m3, int n)
+{
+	int i, j, k;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			m3[i][j] = 0;
+			for (k = 0; k < n; k++)
+			{
+				m3[i][j] += (m1[i][k]*m2[k][j]);
+			}
 		}
 	}
 	return m3;
